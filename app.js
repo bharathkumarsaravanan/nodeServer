99c4c0a38ded8da51a4310386feb0103ad040b76
bharathsaravanan2001 <bharathsaravananofficial@gmail.com>
2023-01-20 17:03:40 +0530

    third commit

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.json());
 app.use(cors());
 
-app.get('/',function(req,res){
+app.get('/home',function(req,res){
     knex('users')
     .select('*')
     .then((e) => res.send(e))
