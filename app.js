47b927346b3351a723c6a1e3cdc1b56f06e5f70d
bharathsaravanan2001 <bharathsaravananofficial@gmail.com>
2023-01-22 15:08:00 +0530

    errrrr

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ const knex = require('knex')({
 });
 
 const corsOptions ={
-    origin:'http://localhost:3000', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
