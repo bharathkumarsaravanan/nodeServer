f2af18cfb40ded90e16856c647f80f86bf708f0d
bharathsaravanan2001 <bharathsaravananofficial@gmail.com>
2023-01-22 15:50:59 +0530

    res

--- booking.js
+++ booking.js
@@ -62,11 +62,6 @@ router.post('/home', function(req,res){
 })
 
 router.post('/home/superagent/signup', function(req,res){
-    res.setHeader("Access-Control-Allow-Origin", "*")
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Max-Age", "1800");
-    res.setHeader("Access-Control-Allow-Headers", "content-type");
-    res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
     var body = req.body;
     knex('users')
     .select('*')
